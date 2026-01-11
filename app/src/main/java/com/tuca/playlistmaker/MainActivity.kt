381945a323322640db427bebeba360b41ed1cbed
tuca <pectopa4pda@yandex.ru>
2026-01-11 11:12:38 +0300

    first commit

--- app/src/main/java/com/tuca/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/tuca/playlistmaker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.tuca.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchButton: Button = findViewById(R.id.search)
+        val libraryButton: Button = findViewById(R.id.library)
+        val settingsButton: Button = findViewById(R.id.settings)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        libraryButton.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
+
